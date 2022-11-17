4b172fb43239fc56ed735b4cbc3a517f200fbe50
LOHITH SB <LOHITH SB@LAPTOP-HVRVGTB7>
2022-11-17 12:16:50 +0530

    first commit

---
+++ Day-1ConnectingeclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,13 @@
+package org.tnsindia.gitdemo;
+
+//Demo on to eclipse with GitHub
+
+/*..1.Create a class and print a statement..*/
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNS India Foundation");
+
+	}
+
+}
