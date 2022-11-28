122a2783f79fef5597c335e890ef6b6d1e688509
LOHITH SB <LOHITH SB@LAPTOP-HVRVGTB7>
2022-11-28 17:56:45 +0530

    first commit

---
+++ Day-3Operator_And_Control-Statement/src/org/tnsindia/jumpingstatements/ReturnDemo.java
@@ -0,0 +1,25 @@
+package org.tnsindia.jumpingstatements;
+
+public class ReturnDemo {
+	
+	static int add(int num1,int num2)
+	{
+		if(num1>num2)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+
+	
+
+	public static void main(String[] args) {
+		System.out.println(add(15,17));
+
+	}
+
+}
